42c1e5acfa26a7ba07c412030f5a5ed26f16bda7
Nolgath <nolgathgraphics@gmail.com>
2025-10-16 16:46:46 +0100

    Initial commit – current working version

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, request, send_file, Response
+from flask import Flask, render_template, request, send_file, Response, make_response
 from io import BytesIO
 import pandas as pd
 from equipment_export import equipment_export
 from conditionreports import conditionreports
 from apscheduler.schedulers.background import BackgroundScheduler
 from download_excel import download_excel_stock,download_excel_sales
-from kpis import n_of_cars, brands_available, count_p_brand, units_sold
+from kpis import n_of_cars, brands_available, count_p_brand, units_sold, models_available, average_sell_price
 import requests
 import os
 
@@ -20,18 +20,36 @@ app.config['DEBUG'] = True
 @app.route("/", methods=["GET", "POST"])
 def home():
     brand_selected = request.form.get("brand", "")
+    model_selected = request.form.get("model", "")
+
+    # if user clicked a model but no brand sent, keep previous brand
+    if not brand_selected and model_selected:
+        brand_selected = request.cookies.get("brand_selected", "")
+
     total_cars = n_of_cars()
     brands = brands_available()
-    brand_count = count_p_brand(brand_selected) if brand_selected else ""
-    units_sold_brand = units_sold(brand_selected) if brand_selected else ""
-    return render_template(
+    models = models_available(brand_selected) if brand_selected else ""
+    brand_count = count_p_brand(model_selected) if model_selected else ""
+    units_sold_brand = units_sold(model_selected) if model_selected else ""
+    avg_selling_price = average_sell_price(model_selected) if model_selected else ""
+
+    response =  make_response(render_template(
         "index.html",
         total_cars=total_cars,
         brands=brands,
         brand_count=brand_count,
         brand_selected=brand_selected,
-        units_sold_brand=units_sold_brand
-    )
+        model_selected=model_selected,
+        units_sold_brand=units_sold_brand,
+        models=models,
+        avg_selling_price=avg_selling_price
+    ))
+
+    # save the selected brand in a cookie for later POSTs
+    if brand_selected:
+        response.set_cookie("brand_selected", brand_selected)
+
+    return response
 
 @app.route("/equipment_export", methods=["GET", "POST"])
 def equipment_export_route():
--- kpis.py
+++ kpis.py
@@ -19,6 +19,7 @@ valid_brands = [
 df = pd.read_excel('stock_list.xlsx')
 df = df[df['Hersteller'].isin(valid_brands)]
 df_sales = pd.read_excel('sales_list.xlsx')
+df_sales = df_sales[df_sales['VK (Netto)'] > 0]
 
 #------------STOCK LIST-------------------------------
 def n_of_cars():
@@ -31,17 +32,24 @@ def brands_available():
     brands_list.tolist()
     return brands_list
 
-def count_p_brand(brand):
-    brand_count = df['Hersteller'].value_counts()
+#---Models gets selected for the brand selected
+def models_available(brand):
+    models = df.loc[df['Hersteller'] == brand, 'Modell'].unique()
+    return models.tolist()
+
+def count_p_brand(model):
+    brand_count = df['Modell'].value_counts()
     brand_count = brand_count.to_dict()
-    return brand_count.get(brand, 0)
+    return brand_count.get(model, 0)
 
 #------------SALES LIST-------------------------------
 
-def units_sold(brand):
-    units_sales = df_sales['Hersteller'].value_counts()
+def units_sold(model):
+    units_sales = df_sales['Modell'].value_counts()
     units_sales = units_sales.to_dict()
-    return units_sales.get(brand, 0)
+    return units_sales.get(model, 0)
 
-
-
+def average_sell_price(model):
+    avg_per_brand = df_sales.groupby('Modell')['VK (Netto)'].mean().round(2)
+    avg_dict = avg_per_brand.to_dict()
+    return avg_dict.get(model, 0)
